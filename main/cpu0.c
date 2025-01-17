5a56d7ce4498a6f643d7005243e4d254ea81f284
ashkorehennessy <ashkorehennessy@gmail.com>
2025-01-17 11:45:23 +0800

    first commit: Display ov2640 image on the ILI9341

---
+++ main/cpu0.c
@@ -0,0 +1,352 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_timer.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "lvgl.h"
+#include "esp_camera.h"
+
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_ILI9341
+#include "esp_lcd_ili9341.h"
+#elif CONFIG_EXAMPLE_LCD_CONTROLLER_GC9A01
+#include "esp_lcd_gc9a01.h"
+#endif
+#include <driver/i2c_master.h>
+
+static const char *TAG = "example";
+
+// Using SPI2 in the example
+#define LCD_HOST    SPI2_HOST
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ     (20 * 1000 * 1000)
+#define EXAMPLE_LCD_BK_LIGHT_ON_LEVEL  1
+#define EXAMPLE_LCD_BK_LIGHT_OFF_LEVEL !EXAMPLE_LCD_BK_LIGHT_ON_LEVEL
+#define EXAMPLE_PIN_NUM_SCLK           3
+#define EXAMPLE_PIN_NUM_MOSI           45
+#define EXAMPLE_PIN_NUM_MISO           46
+#define EXAMPLE_PIN_NUM_LCD_DC         47
+#define EXAMPLE_PIN_NUM_LCD_RST        21
+#define EXAMPLE_PIN_NUM_LCD_CS         14
+#define EXAMPLE_PIN_NUM_BK_LIGHT       0
+
+// The pixel number in horizontal and vertical
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_ILI9341
+#define EXAMPLE_LCD_H_RES              240
+#define EXAMPLE_LCD_V_RES              320
+#elif CONFIG_EXAMPLE_LCD_CONTROLLER_GC9A01
+#define EXAMPLE_LCD_H_RES              240
+#define EXAMPLE_LCD_V_RES              240
+#endif
+// Bit number used to represent command and parameter
+#define EXAMPLE_LCD_CMD_BITS           8
+#define EXAMPLE_LCD_PARAM_BITS         8
+
+#define EXAMPLE_LVGL_TICK_PERIOD_MS    2
+#define EXAMPLE_LVGL_TASK_MAX_DELAY_MS 10
+#define EXAMPLE_LVGL_TASK_MIN_DELAY_MS 1
+#define EXAMPLE_LVGL_TASK_STACK_SIZE   (8 * 1024)
+#define EXAMPLE_LVGL_TASK_PRIORITY     2
+
+static SemaphoreHandle_t lvgl_mux = NULL;
+static i2c_master_bus_handle_t i2c_handle = NULL;
+
+void lv_draw_sw_rgb565_swap(void * buf, uint32_t buf_size_px)
+{
+
+    uint32_t u32_cnt = buf_size_px / 2;
+    uint16_t * buf16 = buf;
+    uint32_t * buf32 = buf;
+
+    while(u32_cnt >= 8) {
+        buf32[0] = ((buf32[0] & 0xff00ff00) >> 8) | ((buf32[0] & 0x00ff00ff) << 8);
+        buf32[1] = ((buf32[1] & 0xff00ff00) >> 8) | ((buf32[1] & 0x00ff00ff) << 8);
+        buf32[2] = ((buf32[2] & 0xff00ff00) >> 8) | ((buf32[2] & 0x00ff00ff) << 8);
+        buf32[3] = ((buf32[3] & 0xff00ff00) >> 8) | ((buf32[3] & 0x00ff00ff) << 8);
+        buf32[4] = ((buf32[4] & 0xff00ff00) >> 8) | ((buf32[4] & 0x00ff00ff) << 8);
+        buf32[5] = ((buf32[5] & 0xff00ff00) >> 8) | ((buf32[5] & 0x00ff00ff) << 8);
+        buf32[6] = ((buf32[6] & 0xff00ff00) >> 8) | ((buf32[6] & 0x00ff00ff) << 8);
+        buf32[7] = ((buf32[7] & 0xff00ff00) >> 8) | ((buf32[7] & 0x00ff00ff) << 8);
+        buf32 += 8;
+        u32_cnt -= 8;
+    }
+
+    while(u32_cnt) {
+        *buf32 = ((*buf32 & 0xff00ff00) >> 8) | ((*buf32 & 0x00ff00ff) << 8);
+        buf32++;
+        u32_cnt--;
+    }
+
+    if(buf_size_px & 0x1) {
+        uint32_t e = buf_size_px - 1;
+        buf16[e] = ((buf16[e] & 0xff00) >> 8) | ((buf16[e] & 0x00ff) << 8);
+    }
+
+}
+
+/* Task to update uptime */
+void update_uptime_task(void *arg) {
+    
+    lv_obj_t *label = lv_label_create(lv_scr_act());
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    while (1) {
+        char buf[32];
+        uint32_t uptime = esp_log_timestamp();
+        snprintf(buf, sizeof(buf), "Uptime: %lu s", uptime);
+        lv_label_set_text(label, buf);
+    }
+}
+static bool example_notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
+{
+    lv_disp_drv_t *disp_driver = (lv_disp_drv_t *)user_ctx;
+    lv_disp_flush_ready(disp_driver);
+    return false;
+}
+
+static void example_lvgl_flush_cb(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_map)
+{
+    esp_lcd_panel_handle_t panel_handle = (esp_lcd_panel_handle_t) drv->user_data;
+    int offsetx1 = area->x1;
+    int offsetx2 = area->x2;
+    int offsety1 = area->y1;
+    int offsety2 = area->y2;
+    // copy a buffer's content to a specific area of the display
+    esp_lcd_panel_draw_bitmap(panel_handle, offsetx1, offsety1, offsetx2 + 1, offsety2 + 1, color_map);
+}
+
+static void example_increase_lvgl_tick(void *arg)
+{
+    /* Tell LVGL how many milliseconds has elapsed */
+    lv_tick_inc(EXAMPLE_LVGL_TICK_PERIOD_MS);
+}
+
+bool example_lvgl_lock(int timeout_ms)
+{
+    // Convert timeout in milliseconds to FreeRTOS ticks
+    // If `timeout_ms` is set to -1, the program will block until the condition is met
+    const TickType_t timeout_ticks = (timeout_ms == -1) ? portMAX_DELAY : pdMS_TO_TICKS(timeout_ms);
+    return xSemaphoreTakeRecursive(lvgl_mux, timeout_ticks) == pdTRUE;
+}
+
+void example_lvgl_unlock(void)
+{
+    xSemaphoreGiveRecursive(lvgl_mux);
+}
+
+static void example_lvgl_port_task(void *arg)
+{
+    ESP_LOGI(TAG, "Starting LVGL task");
+    uint32_t task_delay_ms = EXAMPLE_LVGL_TASK_MAX_DELAY_MS;
+    while (1) {
+        // Lock the mutex due to the LVGL APIs are not thread-safe
+        if (example_lvgl_lock(-1)) {
+            task_delay_ms = lv_timer_handler();
+            // Release the mutex
+            example_lvgl_unlock();
+        }
+        if (task_delay_ms > EXAMPLE_LVGL_TASK_MAX_DELAY_MS) {
+            task_delay_ms = EXAMPLE_LVGL_TASK_MAX_DELAY_MS;
+        } else if (task_delay_ms < EXAMPLE_LVGL_TASK_MIN_DELAY_MS) {
+            task_delay_ms = EXAMPLE_LVGL_TASK_MIN_DELAY_MS;
+        }
+        vTaskDelay(pdMS_TO_TICKS(task_delay_ms));
+    }
+}
+
+void app_main(void)
+{
+    const i2c_master_bus_config_t i2c_config = {
+        .i2c_port = 1,
+        .sda_io_num = GPIO_NUM_4,
+        .scl_io_num = GPIO_NUM_5,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .flags.enable_internal_pullup = true,
+    };
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_config, &i2c_handle));
+    const camera_config_t camera_config = {                                   
+        .pin_pwdn = GPIO_NUM_NC,         
+        .pin_reset = GPIO_NUM_NC,        
+        .pin_xclk = GPIO_NUM_15,     
+        .pin_sccb_sda = GPIO_NUM_NC,     
+        .pin_sccb_scl = GPIO_NUM_NC,     
+        .pin_d7 = GPIO_NUM_16,         
+        .pin_d6 = GPIO_NUM_17,         
+        .pin_d5 = GPIO_NUM_18,         
+        .pin_d4 = GPIO_NUM_12,         
+        .pin_d3 = GPIO_NUM_10,         
+        .pin_d2 = GPIO_NUM_8,         
+        .pin_d1 = GPIO_NUM_9,         
+        .pin_d0 = GPIO_NUM_11,         
+        .pin_vsync = GPIO_NUM_6,   
+        .pin_href = GPIO_NUM_7,    
+        .pin_pclk = GPIO_NUM_13,     
+        .xclk_freq_hz = 16000000,        
+        .ledc_timer = LEDC_TIMER_0,      
+        .ledc_channel = LEDC_CHANNEL_0,  
+        .pixel_format = PIXFORMAT_RGB565,
+        .frame_size = FRAMESIZE_240X240, 
+        .jpeg_quality = 0,              
+        .fb_count = 2,                   
+        .fb_location = CAMERA_FB_IN_PSRAM,
+        .sccb_i2c_port = 1,    
+    };
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        return;
+    }
+    sensor_t *s = esp_camera_sensor_get();
+    s->set_vflip(s, 0);
+    s->set_hmirror(s, 0);
+    ESP_LOGI(TAG, "Camera Init done");
+
+    static lv_disp_draw_buf_t disp_buf; // contains internal graphic buffer(s) called draw buffer(s)
+    static lv_disp_drv_t disp_drv;      // contains callback functions
+
+    ESP_LOGI(TAG, "Turn off LCD backlight");
+    gpio_config_t bk_gpio_config = {
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = 1ULL << EXAMPLE_PIN_NUM_BK_LIGHT
+    };
+    ESP_ERROR_CHECK(gpio_config(&bk_gpio_config));
+
+    ESP_LOGI(TAG, "Initialize SPI bus");
+    spi_bus_config_t buscfg = {
+        .sclk_io_num = EXAMPLE_PIN_NUM_SCLK,
+        .mosi_io_num = EXAMPLE_PIN_NUM_MOSI,
+        .miso_io_num = EXAMPLE_PIN_NUM_MISO,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = EXAMPLE_LCD_H_RES * 80 * sizeof(uint16_t),
+    };
+    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
+
+    ESP_LOGI(TAG, "Install panel IO");
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_spi_config_t io_config = {
+        .dc_gpio_num = EXAMPLE_PIN_NUM_LCD_DC,
+        .cs_gpio_num = EXAMPLE_PIN_NUM_LCD_CS,
+        .pclk_hz = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
+        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,
+        .lcd_param_bits = EXAMPLE_LCD_PARAM_BITS,
+        .spi_mode = 0,
+        .trans_queue_depth = 10,
+        .on_color_trans_done = example_notify_lvgl_flush_ready,
+        .user_ctx = &disp_drv,
+    };
+    // Attach the LCD to the SPI bus
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
+
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .reset_gpio_num = EXAMPLE_PIN_NUM_LCD_RST,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_BGR,
+        .bits_per_pixel = 16,
+    };
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_ILI9341
+    ESP_LOGI(TAG, "Install ILI9341 panel driver");
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ili9341(io_handle, &panel_config, &panel_handle));
+#elif CONFIG_EXAMPLE_LCD_CONTROLLER_GC9A01
+    ESP_LOGI(TAG, "Install GC9A01 panel driver");
+    ESP_ERROR_CHECK(esp_lcd_new_panel_gc9a01(io_handle, &panel_config, &panel_handle));
+#endif
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_GC9A01
+    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
+#endif
+    ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, true, false));
+
+    // user can flush pre-defined pattern to the screen before we turn on the screen or backlight
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+    ESP_LOGI(TAG, "Turn on LCD backlight");
+    gpio_set_level(EXAMPLE_PIN_NUM_BK_LIGHT, EXAMPLE_LCD_BK_LIGHT_ON_LEVEL);
+
+    ESP_LOGI(TAG, "Initialize LVGL library");
+    lv_init();
+    // alloc draw buffers used by LVGL
+    // it's recommended to choose the size of the draw buffer(s) to be at least 1/10 screen sized
+    lv_color_t *buf1 = heap_caps_malloc(EXAMPLE_LCD_H_RES * 20 * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf1);
+    lv_color_t *buf2 = heap_caps_malloc(EXAMPLE_LCD_H_RES * 20 * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf2);
+    // initialize LVGL draw buffers
+    lv_disp_draw_buf_init(&disp_buf, buf1, buf2, EXAMPLE_LCD_H_RES * 20);
+
+    ESP_LOGI(TAG, "Register display driver to LVGL");
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.hor_res = EXAMPLE_LCD_H_RES;
+    disp_drv.ver_res = EXAMPLE_LCD_V_RES;
+    disp_drv.flush_cb = example_lvgl_flush_cb;
+    disp_drv.drv_update_cb = NULL;
+    disp_drv.draw_buf = &disp_buf;
+    disp_drv.user_data = panel_handle;
+    // lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
+    lv_disp_drv_register(&disp_drv);
+
+    ESP_LOGI(TAG, "Install LVGL tick timer");
+    // Tick interface for LVGL (using esp_timer to generate 2ms periodic event)
+    const esp_timer_create_args_t lvgl_tick_timer_args = {
+        .callback = &example_increase_lvgl_tick,
+        .name = "lvgl_tick"
+    };
+    esp_timer_handle_t lvgl_tick_timer = NULL;
+    ESP_ERROR_CHECK(esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_tick_timer, EXAMPLE_LVGL_TICK_PERIOD_MS * 1000));
+
+    lvgl_mux = xSemaphoreCreateRecursiveMutex();
+    assert(lvgl_mux);
+    ESP_LOGI(TAG, "Create LVGL task");
+    xTaskCreate(example_lvgl_port_task, "LVGL", EXAMPLE_LVGL_TASK_STACK_SIZE, NULL, EXAMPLE_LVGL_TASK_PRIORITY, NULL);
+    ESP_LOGI(TAG, "Display LVGL Meter Widget");
+    uint32_t cam_buff_size = 240 * 240 * 2;
+    uint8_t *cbuf = heap_caps_malloc(cam_buff_size, MALLOC_CAP_SPIRAM);
+    camera_fb_t *pic;
+    if (example_lvgl_lock(-1)) {
+        lv_draw_label_dsc_t label_dsc;
+        lv_draw_label_dsc_init(&label_dsc);
+        label_dsc.color = lv_palette_main(LV_PALETTE_BLUE);
+        lv_obj_t * canvas = lv_canvas_create(lv_scr_act());
+        lv_canvas_set_buffer(canvas, cbuf, 240, 240, LV_IMG_CF_TRUE_COLOR);
+        lv_obj_center(canvas);
+        lv_canvas_fill_bg(canvas, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_OPA_COVER);
+        lv_canvas_draw_text(canvas, 0, 0, 240, &label_dsc, "Some text on text canvas ssssssssssssssssssssssssssssssss");
+        // Release the mutex
+        example_lvgl_unlock();
+    }
+    vTaskDelay(5 / portTICK_PERIOD_MS);
+    lv_obj_t * canvas = lv_canvas_create(lv_scr_act());
+    lv_canvas_set_buffer(canvas, cbuf, 240, 240, LV_IMG_CF_TRUE_COLOR);
+    lv_obj_center(canvas);
+    while(1){
+        pic = esp_camera_fb_get();
+        if (pic) {
+            esp_camera_fb_return(pic);
+            if (example_lvgl_lock(-1)) {
+                memcpy(cbuf, pic->buf, cam_buff_size);
+                lv_obj_invalidate(canvas);
+                example_lvgl_unlock();
+            }
+        } else {
+            ESP_LOGE(TAG, "Frame buffer could not be acquired");
+        }
+        vTaskDelay(1);
+    }
+}
